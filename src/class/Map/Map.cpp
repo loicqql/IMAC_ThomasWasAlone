6ab94660bb12e4586bebe7d2405e6bc64aea15c2
Greg0s <gregoire.tinnes@outlook.fr>
2022-05-24 12:14:31 +0200

    getLeaves return vector<Block>

--- src/class/Map/Map.cpp
+++ src/class/Map/Map.cpp
@@ -31,38 +31,36 @@ void Map::buildMap(vector<Block*> b){
 
 }
 
-vector<Elt *> Map::getLeaves(vector<Elt *> leaves, Elt * node){
+// vector<Elt *> Map::getLeaves(vector<Elt *> leaves, Elt * node){
+//     if(node->isLeaf() && node->getNbBlocks() > 0 && node != 0x0){
+//         leaves.push_back(node);
+//         cout << "added a leaf" << endl;
+//     }else if(!node->isLeaf()){
+//         getLeaves(leaves, node->getChildA());
+//         getLeaves(leaves, node->getChildB());
+//         getLeaves(leaves, node->getChildC());
+//         getLeaves(leaves, node->getChildD());
+//     }
+//     return leaves;
+// }
+
+vector<Block*> * Map::getLeaves(vector<Block*> * leaves, Elt * node){
+    
     if(node->isLeaf() && node->getNbBlocks() > 0 && node != 0x0){
-        leaves.push_back(node);
-        cout << "added a leaf" << endl;
-    }else if(!node->isLeaf()){
+        Block * blocks = node->getBlocks();
+        for(int i = 0 ; i < node->getNbBlocks() ; i++){
+            leaves->push_back(&(blocks[i]));
+        }   
+    }
+    if(!node->isLeaf()){
         getLeaves(leaves, node->getChildA());
         getLeaves(leaves, node->getChildB());
         getLeaves(leaves, node->getChildC());
         getLeaves(leaves, node->getChildD());
     }
+
     return leaves;
 }
-
-// vector<Block*> * Map::getLeaves(Elt * node){
-//     vector<Block*>  * bLeaves;
-//     if(node->isLeaf()){
-//         Block * blocks = node->getBlocks();
-//         for(int i = 0 ; i < node->getNbBlocks() ; i++){
-//             bLeaves->push_back(&(blocks[i]));
-//         }   
-//     }
-//     if(!node->isLeaf()){
-//         getLeaves(bLeaves, node->getChildA());
-//         getLeaves(bLeaves, node->getChildB());
-//         getLeaves(bLeaves, node->getChildC());
-//         getLeaves(bLeaves, node->getChildD());
-//     }
-
-//     cout << "hjsdfkbfskj" << bLeaves->size() << endl;
-
-//     return bLeaves;
-// }
 
 vector<Block*> Map::search(Vector pos){
     return r->search(pos);
@@ -99,21 +97,25 @@ vector<Block*> Map::search(Vector pos){
 //     return blocks;
 // }
 
-/*void Map::drawMap(vector<Elt *> leaves){
+void Map::drawMap(vector<Block *> leaves){
     // for(uint i = 0 ; i < getLeaves(bLeaves, r).size() ; i++){
     //     bLeaves->push_back(getLeaves(bLeaves, r)[i]);
     // }
-    cout << "is empty? " << leaves.size() << endl;
+    //cout << "is empty? " << leaves.size() << endl;
     //cout << "leaves : " << bLeaves[0]->getBlocks()[0].getBox()->getY() << endl;
     
     //draw.render(&(leaves[0].getBlocks()[0]));
 
-    // for(uint i = 0 ; i < leaves->size() ; i++){
-    //     for(int j = 0 ; j < leaves->at(i)->getNbBlocks() ; j++){
-    //         draw.render(leaves->at(i)->getBlocks()[j]);
+    // vector <Block*> allBlocks;
+
+    // for(uint i = 0 ; i < leaves.size() ; i++){
+    //     for(int j = 0 ; j < leaves.at(i)->getNbBlocks() ; j++){
+    //         allBlocks.push_back(leaves[i]->getBlocks()[i]);
     //     }
     // }
-}*/
+
+    draw.render(leaves);
+}
 
 vector<Elt *> Map::allElt(){
     vector<Elt *> elts;
